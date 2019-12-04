e6b763f9657bd6b30d991d01c0a4e64bde1feb49
Mitchell Irvin <mitchell.k.irvin@gmail.com>
2019-12-04 10:39:48 -0500

    adding YourLinkedDeque seed

---
+++ src/main/java/katas/datastructures/deque/linkeddeque/YourLinkedDeque.java
@@ -0,0 +1,39 @@
+package katas.datastructures.deque.linkeddeque;
+
+import katas.datastructures.deque.Deque;
+
+public class YourLinkedDeque<E> implements Deque<E> {
+
+    public void addFirst(E element) {
+
+    }
+
+    public void addLast(E element) {
+
+    }
+
+    public E removeFirst() {
+        return null;
+    }
+
+    public E removeLast() {
+        return null;
+    }
+
+    public E peekFirst() {
+        return null;
+    }
+
+    public E peekLast() {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+}
